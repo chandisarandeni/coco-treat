92c2c332a418fbba57dc552d706e9dd5bf01256e
chandisarandeni <chandisarandeni2003@gmail.com>
2025-08-21 20:38:49 +0530

    init: motor testing code ✅

---
+++ testing-codes/motor-testing/motor-testing.ino
@@ -0,0 +1,18 @@
+#define enb 13
+#define in4 12
+#define in3 11
+#define in2 10
+#define in1 9
+#define ena 8
+
+
+void setup() {
+  pinMode(ena, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in1, OUTPUT);
+}
+
+void loop() {
+}
